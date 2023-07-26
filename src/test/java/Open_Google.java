f489e555f5536152d98157db49f5dc6bedfc8b26
RomanG <roma@gmail.com>
2023-07-26 18:06:48 +0300

    add sleep

--- src/test/java/Open_Google.java
+++ src/test/java/Open_Google.java
@@ -5,8 +5,7 @@ import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 import static com.codeborne.selenide.Condition.text;
-import static com.codeborne.selenide.Selenide.$;
-import static com.codeborne.selenide.Selenide.open;
+import static com.codeborne.selenide.Selenide.*;
 
 public class Open_Google {
     @BeforeAll
@@ -20,6 +19,7 @@ public class Open_Google {
         open("https://www.google.com/");
         $("[name=q]").setValue("selenide").pressEnter();
         $("[id=search]").shouldHave(text("https://ru.selenide.org"));
+        sleep(777);
     }
 
 }
